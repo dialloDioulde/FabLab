7242849f05b90271d3c451acebb0b7c90d9f8731
kristi <kristimihali3@gmail.com>
2020-04-05 21:00:42 +0200

    homepage

--- siteWeb/views.py
+++ siteWeb/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Material, Type
+from django.core.paginator import Paginator
+
 
 # Create your views here.
+def homepage(request):
+    materials = Material.objects.all()
+
+    search_term = ""
+
+    if 'search' in request.GET:
+        search_term = request.GET['search']
+        materials = (materials.filter(name__icontains=search_term) | materials.filter(barcode__icontains=search_term))
+
+        if search_term == "":
+            search_term = "No match."
+
+    paginator = Paginator(materials, 8)
+
+    page = request.GET.get('page')
+
+    materials = paginator.get_page(page)
+
+    return render(request=request,
+                  template_name="siteWeb/home.html",
+                  context={"materials": materials, "search_term": search_term})
