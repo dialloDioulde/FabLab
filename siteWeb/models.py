ad39652b85bb151bacf3123c220639969743fa29
kristi <kristimihali3@gmail.com>
2020-05-14 09:58:05 +0200

    alternate loans tables

--- siteWeb/models.py
+++ siteWeb/models.py
@@ -10,8 +10,6 @@ from projetFablab.utils import unique_slug_generator
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
-    one_click_purchasing = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
